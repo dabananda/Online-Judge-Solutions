9aafbd36cb799b9cfda07de5835801b61ddb1cf2
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-01-13 11:57:07 +0600

    Codeforces 155A - I_love_\%username\%

---
+++ CodeForces/155A - I_love_%username%/cmake-build-debug/CMakeFiles/155A___I_love__username_.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Online-Judge-Solutions/CodeForces/155A - I_love_%username%/main.cpp" "D:/Online-Judge-Solutions/CodeForces/155A - I_love_%username%/cmake-build-debug/CMakeFiles/155A___I_love__username_.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CodeForces/155A - I_love_%username%/cmake-build-debug/CMakeFiles/155A___I_love__username_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "155A___I_love__username_.exe"
+  "155A___I_love__username_.exe.manifest"
+  "155A___I_love__username_.pdb"
+  "CMakeFiles/155A___I_love__username_.dir/main.cpp.obj"
+  "lib155A___I_love__username_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/155A___I_love__username_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
