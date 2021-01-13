f575bfc478a8fa608295cfdcf4ad39b1595a4f66
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-01-13 12:24:58 +0600

    228A - Is your horseshoe on the other hoof?

---
+++ CodeForces/288A - Is your horseshoe on the other hoof/cmake-build-debug/CMakeFiles/288A___Is_your_horseshoe_on_the_other_hoof.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Online-Judge-Solutions/CodeForces/288A - Is your horseshoe on the other hoof/main.cpp" "D:/Online-Judge-Solutions/CodeForces/288A - Is your horseshoe on the other hoof/cmake-build-debug/CMakeFiles/288A___Is_your_horseshoe_on_the_other_hoof.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CodeForces/288A - Is your horseshoe on the other hoof/cmake-build-debug/CMakeFiles/288A___Is_your_horseshoe_on_the_other_hoof.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "288A___Is_your_horseshoe_on_the_other_hoof.exe"
+  "288A___Is_your_horseshoe_on_the_other_hoof.exe.manifest"
+  "288A___Is_your_horseshoe_on_the_other_hoof.pdb"
+  "CMakeFiles/288A___Is_your_horseshoe_on_the_other_hoof.dir/main.cpp.obj"
+  "lib288A___Is_your_horseshoe_on_the_other_hoof.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/288A___Is_your_horseshoe_on_the_other_hoof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
