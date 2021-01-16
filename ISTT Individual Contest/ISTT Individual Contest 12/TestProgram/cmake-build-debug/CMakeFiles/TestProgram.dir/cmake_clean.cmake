4879055b10d238be31e15ee082676de46738118c
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-01-16 23:37:11 +0600

    ISTT Individual Contest 12

---
+++ ISTT Individual Contest/ISTT Individual Contest 12/TestProgram/cmake-build-debug/CMakeFiles/TestProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestProgram.dir/main.cpp.obj"
+  "TestProgram.exe"
+  "TestProgram.exe.manifest"
+  "TestProgram.pdb"
+  "libTestProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
