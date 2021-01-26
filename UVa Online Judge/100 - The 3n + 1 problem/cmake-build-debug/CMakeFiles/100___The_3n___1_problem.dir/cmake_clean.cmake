e1afdefbf8f597261d919253e780435226b1b966
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-01-26 17:53:03 +0600

    100 - The 3n + 1 problem

---
+++ UVa Online Judge/100 - The 3n + 1 problem/cmake-build-debug/CMakeFiles/100___The_3n___1_problem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "100___The_3n___1_problem.exe"
+  "100___The_3n___1_problem.exe.manifest"
+  "100___The_3n___1_problem.pdb"
+  "CMakeFiles/100___The_3n___1_problem.dir/main.cpp.obj"
+  "lib100___The_3n___1_problem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/100___The_3n___1_problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
