5b615d8de5cf34062a2dd30c95ceee6f07f25304
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-02-20 19:54:28 +0600

    494 - Kindergarten Counting Game

---
+++ UVa Online Judge/494 - Kindergarten Counting Game/cmake-build-debug/CMakeFiles/494___Kindergarten_Counting_Game.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/My Codes & Projects/Codes/Online-Judge-Solutions/UVa Online Judge/494 - Kindergarten Counting Game/main.cpp" "D:/My Codes & Projects/Codes/Online-Judge-Solutions/UVa Online Judge/494 - Kindergarten Counting Game/cmake-build-debug/CMakeFiles/494___Kindergarten_Counting_Game.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ UVa Online Judge/494 - Kindergarten Counting Game/cmake-build-debug/CMakeFiles/494___Kindergarten_Counting_Game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "494___Kindergarten_Counting_Game.exe"
+  "494___Kindergarten_Counting_Game.exe.manifest"
+  "494___Kindergarten_Counting_Game.pdb"
+  "CMakeFiles/494___Kindergarten_Counting_Game.dir/main.cpp.obj"
+  "lib494___Kindergarten_Counting_Game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/494___Kindergarten_Counting_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
