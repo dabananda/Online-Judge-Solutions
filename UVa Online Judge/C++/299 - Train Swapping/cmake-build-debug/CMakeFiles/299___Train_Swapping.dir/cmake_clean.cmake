4297ba72fe528be973729b5464796362dd31e7da
Dabananda Mitra <67568795+dabananda@users.noreply.github.com>
2021-03-05 10:08:04 +0600

    299 - Train Swapping

---
+++ UVa Online Judge/C++/299 - Train Swapping/cmake-build-debug/CMakeFiles/299___Train_Swapping.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "299___Train_Swapping.exe"
+  "299___Train_Swapping.exe.manifest"
+  "299___Train_Swapping.pdb"
+  "CMakeFiles/299___Train_Swapping.dir/main.cpp.obj"
+  "lib299___Train_Swapping.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/299___Train_Swapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
