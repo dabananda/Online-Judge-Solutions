7a047d1f279b63716d42e2b6be4326b7c96ce128
Dabananda Mitra <dmitraofficial@gmail.com>
2021-06-09 10:32:47 +0600

    Ice Cream Parlor

---
+++ HackerRank/C++/Ice-Cream-Parlor/cmake-build-debug/CMakeFiles/Ice_Cream_Parlor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ice_Cream_Parlor.dir/main.cpp.obj"
+  "Ice_Cream_Parlor.exe"
+  "Ice_Cream_Parlor.exe.manifest"
+  "Ice_Cream_Parlor.pdb"
+  "libIce_Cream_Parlor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ice_Cream_Parlor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
